1b2c0fd9ed940289d6f5ab884734c6d4fd0c6765
= <=>
2020-05-12 15:59:51 +0200

    whatever

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-const fs = require('fs');
-const options = {
-  key: fs.readFileSync('/usr/local/psa/var/modules/letsencrypt/etc/archive/spiralingdown.de/privkey1.pem'),
-  cert: fs.readFileSync('/usr/local/psa/var/modules/letsencrypt/etc/archive/spiralingdown.de/fullchain1.pem'),
-};
-/* const options = {
-  key: fs.readFileSync('../SSL/privkey1.pem'),
-  cert: fs.readFileSync('../SSL/cert1.pem'),
-  ca: fs.readFileSync('../SSL/chain1.pem'),
-}; */
-const Express = require('express')();
-//const Https = require('http').Server(options, Express);
-const Https = require('https').Server(Express, options);
-const Socketio = require('socket.io')(Https, { origins: 'http://localhost:8080' });
+var fs = require('fs');
+var app = require('express')();
+var https = require('https');
+var server = https.createServer(
+  {
+    key: fs.readFileSync('/usr/local/psa/var/modules/letsencrypt/etc/archive/spiralingdown.de/privkey1.pem'),
+    cert: fs.readFileSync('/usr/local/psa/var/modules/letsencrypt/etc/archive/spiralingdown.de/fullchain1.pem'),
+  },
+  app
+);
+server.listen(3000);
+
+var io = require('socket.io').listen(server);
 
 let testMessage = 'Das ist ein Test';
 
-Socketio.on('connection', (socket) => {
+io.sockets.on('connection', (socket) => {
   socket.emit('testMessage', testMessage);
 });
 
-Https.listen(3000, () => {
-  console.log('listening at :3000...');
+app.get('/', (request, response) => {
+  console.log('app.get');
 });
