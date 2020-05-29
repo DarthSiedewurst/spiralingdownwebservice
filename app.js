a7973636a66531a29aa0da34f5534b4921302103
= <=>
2020-05-29 21:31:04 +0200

    k

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 var fs = require('fs');
 var app = require('express')();
-var https = require('https');
+var https = require('http');
 var server = https.createServer(
-  {
+  /*   {
     key: fs.readFileSync('./certs/privkey1.pem'),
     cert: fs.readFileSync('./certs/fullchain1.pem'),
-  },
+  }, */
   app
 );
-/* var server = https.createServer(
-  {
-    key: fs.readFileSync('../SSL/privkey1.pem'),
-    cert: fs.readFileSync('../SSL/fullchain1.pem'),
-  },
-  app
-); */
 server.listen(3000);
 
 var io = require('socket.io').listen(server);
 
-let testMessage = 'Das ist ein Test';
+let testMessage = 'Das ist kein Test';
 
 io.sockets.on('connection', (socket) => {
   socket.emit('testMessage', testMessage);
