e01ee87070db37ded8ff19f0d581d106a3f24787
= <=>
2020-05-30 15:48:08 +0200

    works local and hopefully in prod

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-var fs = require('fs');
-var app = require('express')();
-var https = require('https');
-var server = https.createServer(
-    {
-    key: fs.readFileSync('./certs/privkey1.pem'),
-    cert: fs.readFileSync('./certs/fullchain1.pem'),
-  },
-  app
-);
+var fs = require("fs");
+var app = require("express")();
+var options = {};
+var https = null;
+try {
+  options = {
+    key: fs.readFileSync("./certs/privkey1.pem"),
+    cert: fs.readFileSync("./certs/fullchain1.pem"),
+  };
+  https = require("https");
+} catch (e) {
+  https = require("http");
+}
+
+var server = https.createServer(options, app);
 server.listen(3000);
 
-var io = require('socket.io').listen(server);
+var io = require("socket.io").listen(server);
 
-let testMessage = 'Das ist kein Test';
+let testMessage = "Das ist kein Test";
 
-io.sockets.on('connection', (socket) => {
-  socket.emit('testMessage', testMessage);
+io.sockets.on("connection", (socket) => {
+  socket.emit("testMessage", testMessage);
 });
 
-app.get('/', (request, response) => {
-  console.log('app.get');
+app.get("/", (request, response) => {
+  console.log("app.get");
 });
