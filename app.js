497e2533e5f78b06ceaba732320418678441832e
= <=>
2020-06-24 20:11:20 +0200

    console log for reconnect

--- app.js
+++ app.js
@@ -33,6 +33,15 @@ io.sockets.on("connection", (gameRoom) => {
 
   gameRoom.on("reconnectSocket", (lobby) => {
     gameRoom.join(lobby.lobby);
+    console.log("lobby lobby");
+    console.log(lobby.lobby);
+    console.log("lobby ownlobby");
+    console.log(lobby.ownlobby);
+    console.log("playersUpdated");
+    console.log(io.sockets.adapter.rooms[lobby.lobby].players);
+    console.log("rulesetUpdated");
+    console.log(io.sockets.adapter.rooms[lobby.lobby].ruleset);
+    console.log("nextTurn");
     io.sockets.in(lobby.ownLobby).emit("playersUpdated", io.sockets.adapter.rooms[lobby.lobby].players);
     io.sockets.in(lobby.ownLobby).emit("rulesetUpdated", io.sockets.adapter.rooms[lobby.lobby].ruleset);
     io.sockets.in(lobby.ownLobby).emit("nextTurn", io.sockets.adapter.rooms[lobby.lobby].players);
@@ -45,118 +54,115 @@ io.sockets.on("connection", (gameRoom) => {
     gameRoom.emit("lobbyJoined", lobby);
     io.sockets.in(lobby).emit("playersUpdated", io.sockets.adapter.rooms[lobby].players);
     io.sockets.in(lobby).emit("rulesetUpdated", io.sockets.adapter.rooms[lobby].ruleset);
+  });
 
-    gameRoom.on("startGame", () => {
-      io.sockets.adapter.rooms[lobby].gameStarted = true;
-      io.sockets.in(lobby).emit("gameStarted");
-    });
+  gameRoom.on("startGame", (lobby) => {
+    io.sockets.adapter.rooms[lobby].gameStarted = true;
+    io.sockets.in(lobby).emit("gameStarted");
+  });
 
-    // Players
-    gameRoom.on("addPlayerToSocket", (newPlayer) => {
-      const lobby = newPlayer.lobby;
-      const ownLobby = newPlayer.ownLobby;
-      let players = [];
+  // Players
+  gameRoom.on("addPlayerToSocket", (newPlayer) => {
+    const lobby = newPlayer.lobby;
+    const ownLobby = newPlayer.ownLobby;
+    let players = [];
 
-      if (io.sockets.adapter.rooms.gameStarted) {
-        /* const errorMessage = "Spiel ist schon im Gange";
-        io.sockets.in(lobby).emit("error", errorMessage); */
-        gameRoom.leave(lobby);
-        return;
+    if (io.sockets.adapter.rooms.gameStarted) {
+      /* const errorMessage = "Spiel ist schon im Gange";
+      io.sockets.in(lobby).emit("error", errorMessage); */
+      gameRoom.leave(lobby);
+      return;
+    }
+
+    if (io.sockets.adapter.rooms[lobby].players !== undefined) {
+      players = io.sockets.adapter.rooms[lobby].players;
+    }
+    let error = false;
+    players.forEach((player) => {
+      if (player.color === newPlayer.newPlayer.color) {
+        io.sockets.in(ownLobby).emit("playersNotUpdated", newPlayer.newPlayer.color);
+        error = true;
       }
+    });
+    if (error) {
+      return;
+    }
+    const newPlayers = players;
 
-      if (io.sockets.adapter.rooms[lobby].players !== undefined) {
-        players = io.sockets.adapter.rooms[lobby].players;
+    newPlayer.newPlayer.id = players.length;
+
+    newPlayers.push(newPlayer.newPlayer);
+    players = newPlayers;
+    io.sockets.adapter.rooms[lobby].players = players;
+    io.sockets.in(lobby).emit("playersUpdated", players);
+    io.sockets.in(ownLobby).emit("goToNewGame");
+  });
+
+  gameRoom.on("getPlayerFromSocket", (lobby) => {
+    io.sockets.in(lobby).emit("playersUpdated", io.sockets.adapter.rooms[lobby].players);
+  });
+
+  gameRoom.on("getUpdate", (lobby) => {
+    io.sockets.in(lobby.ownLobby).emit("gotUpdate", io.sockets.adapter.rooms[lobby.lobby].players);
+  });
+
+  // Ruleset
+  gameRoom.on("setRulesetToSocket", (ruleset) => {
+    const lobby = ruleset.lobby;
+    io.sockets.adapter.rooms[lobby].ruleset = ruleset.ruleset;
+
+    io.sockets.in(lobby).emit("rulesetUpdated", io.sockets.adapter.rooms[lobby].ruleset);
+  });
+  // PopUp
+  gameRoom.on("updatePopUpOpen", (lobby) => {
+    io.sockets.in(lobby).emit("popUpUpdated", io.sockets.adapter.rooms[lobby].popUpOpen);
+  });
+  gameRoom.on("okClicked", (lobby) => {
+    io.sockets.in(lobby).emit("okHasBeenClicked");
+    io.sockets.adapter.rooms[lobby].popUpOpen = false;
+  });
+  gameRoom.on("showRuleInSocket", (id) => {
+    const lobby = id.lobby;
+    io.sockets.in(lobby).emit("ruleOpened", {
+      id: id.id,
+      players: io.sockets.adapter.rooms[lobby].players,
+    });
+    io.sockets.adapter.rooms[lobby].popUpOpen = true;
+    io.sockets.in(lobby).emit("popUpUpdated", io.sockets.adapter.rooms[lobby].popUpOpen);
+  });
+  // Dice
+  gameRoom.on("moveInSocket", (payload) => {
+    const lobby = payload.lobby;
+    let players = io.sockets.adapter.rooms[lobby].players;
+
+    players[payload.playerId].tile = players[payload.playerId].tile + payload.roll;
+    if (players[payload.playerId].tile > 72) {
+      players[payload.playerId].tile = 72;
+    }
+    io.sockets.adapter.rooms[lobby].players = players;
+
+    io.sockets.in(lobby).emit("diceWasRolled", { roll: payload.roll, playerId: payload.playerId, players });
+  });
+
+  gameRoom.on("newActivePlayer", (lobby) => {
+    let players = io.sockets.adapter.rooms[lobby].players;
+
+    let id = 0;
+    players.forEach((element) => {
+      if (element.activeTurn) {
+        id = element.id;
       }
-      let error = false;
-      players.forEach((player) => {
-        if (player.color === newPlayer.newPlayer.color) {
-          io.sockets.in(ownLobby).emit("playersNotUpdated", newPlayer.newPlayer.color);
-          error = true;
-        }
-      });
-      if (error) {
-        return;
-      }
-      const newPlayers = players;
-
-      newPlayer.newPlayer.id = players.length;
-
-      newPlayers.push(newPlayer.newPlayer);
-      players = newPlayers;
-      io.sockets.adapter.rooms[lobby].players = players;
-      io.sockets.in(lobby).emit("playersUpdated", players);
-      io.sockets.in(ownLobby).emit("goToNewGame");
     });
 
-    gameRoom.on("getPlayerFromSocket", (lobby) => {
-      io.sockets.in(lobby).emit("playersUpdated", io.sockets.adapter.rooms[lobby].players);
-    });
+    players[id].activeTurn = false;
+    if (id < players.length - 1) {
+      players[id + 1].activeTurn = true;
+    } else {
+      players[0].activeTurn = true;
+    }
 
-    gameRoom.on("getUpdate", (lobby) => {
-      io.sockets.in(lobby.ownLobby).emit("gotUpdate", io.sockets.adapter.rooms[lobby.lobby].players);
-    });
+    io.sockets.adapter.rooms[lobby].players = players;
 
-    // Ruleset
-    gameRoom.on("setRulesetToSocket", (ruleset) => {
-      const lobby = ruleset.lobby;
-      console.log(io.sockets.adapter.rooms);
-      console.log("lobby: " + lobby);
-      console.log("lobbyrooms: " + io.sockets.adapter.rooms[lobby]);
-      io.sockets.adapter.rooms[lobby].ruleset = ruleset.ruleset;
-
-      io.sockets.in(lobby).emit("rulesetUpdated", io.sockets.adapter.rooms[lobby].ruleset);
-    });
-    // PopUp
-    gameRoom.on("updatePopUpOpen", (lobby) => {
-      io.sockets.in(lobby).emit("popUpUpdated", io.sockets.adapter.rooms[lobby].popUpOpen);
-    });
-    gameRoom.on("okClicked", (lobby) => {
-      io.sockets.in(lobby).emit("okHasBeenClicked");
-      io.sockets.adapter.rooms[lobby].popUpOpen = false;
-    });
-    gameRoom.on("showRuleInSocket", (id) => {
-      const lobby = id.lobby;
-      io.sockets.in(lobby).emit("ruleOpened", {
-        id: id.id,
-        players: io.sockets.adapter.rooms[lobby].players,
-      });
-      io.sockets.adapter.rooms[lobby].popUpOpen = true;
-      io.sockets.in(lobby).emit("popUpUpdated", io.sockets.adapter.rooms[lobby].popUpOpen);
-    });
-    // Dice
-    gameRoom.on("moveInSocket", (payload) => {
-      const lobby = payload.lobby;
-      let players = io.sockets.adapter.rooms[lobby].players;
-
-      players[payload.playerId].tile = players[payload.playerId].tile + payload.roll;
-      if (players[payload.playerId].tile > 72) {
-        players[payload.playerId].tile = 72;
-      }
-      io.sockets.adapter.rooms[lobby].players = players;
-
-      io.sockets.in(lobby).emit("diceWasRolled", { roll: payload.roll, playerId: payload.playerId, players });
-    });
-
-    gameRoom.on("newActivePlayer", (lobby) => {
-      let players = io.sockets.adapter.rooms[lobby].players;
-
-      let id = 0;
-      players.forEach((element) => {
-        if (element.activeTurn) {
-          id = element.id;
-        }
-      });
-
-      players[id].activeTurn = false;
-      if (id < players.length - 1) {
-        players[id + 1].activeTurn = true;
-      } else {
-        players[0].activeTurn = true;
-      }
-
-      io.sockets.adapter.rooms[lobby].players = players;
-
-      io.sockets.in(lobby).emit("nextTurn", players);
-    });
+    io.sockets.in(lobby).emit("nextTurn", players);
   });
 });
